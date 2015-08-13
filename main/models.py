d8fbe01e30c77f7b522f27094a6c9dfc0bebe29d
khalifa almuzain <khalifaalmuzain@khalifas-MacBook-Pro.local>
2015-08-13 13:02:02 +0300

    added requirments file

--- main/models.py
+++ main/models.py
@@ -12,6 +12,8 @@ class Cereal(models.Model):
 	name = models.CharField(max_length=30, unique=True)
 	type = models.CharField(max_length=2, null=True)
 	manufacturer = models.ForeignKey('main.Manufacturer', null=True)
+	image = models.ImageField(upload_to="cereal", null=True)
+	info = models.TextField()
 
 
 	def __unicode__(self):
--- project/urls.py
+++ project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -34,4 +36,5 @@ urlpatterns = [
     url(r'^login/$', 'main.views.login_view'),
     url(r'^logout/$', 'main.views.logout_view'),
 
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
